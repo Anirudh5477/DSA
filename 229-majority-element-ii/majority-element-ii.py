61ae52a2ed6fbbe9bfd46d242de444f052eb6e49
Anirudh5477 <am25b003@smail.iitm.ac.in>
2026-07-04 23:52:09 +0530

    Time: 11 ms (34.68%) | Memory: 22.5 MB (53.98%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,23 +1,37 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = {}
         length = len(nums)
         len3 = length//3
-        done = set()
+        if length == 1:
+            return nums
+        count1 = 0
+        count2 = 0
+        candidate1 = None
+        candidate2 = None
+
         for i in nums:
-            if i not in done:
-                if i in count:
-                    count[i] +=1
-                    if count[i] >len3:
-                        done.add(i)
-                        del count[i]
-                else:
-                    count[i] = 1
-                    if count[i] >len3:
-                        done.add(i)
-                        del count[i]
-                        
-        return list(done)
+            if i == candidate1:
+                count1 +=1
+            elif i == candidate2:
+                count2+=1
+            elif count1==0:
+                candidate1 = i
+                count1+=1
+            elif count2 == 0:
+                candidate2 = i
+                count2+=1
+            else:
+                count1 -=1
+                count2-=1
+        if candidate1!=None and candidate2 !=None and nums.count(candidate1)>len3 and nums.count(candidate2)>len3:
+            return [candidate1,candidate2]
+        elif candidate1!=None and nums.count(candidate1) >len3 :
+            return [candidate1]
+        elif nums.count(candidate2)>len3:
+            return[candidate2]
+        else:
+            return []
+        
 
 
         
