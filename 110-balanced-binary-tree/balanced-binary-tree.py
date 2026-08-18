991411b4088f05779f2145ecc0717e3f98c4ae1b
Anirudh5477 <am25b003@smail.iitm.ac.in>
2026-08-19 00:26:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (70.01%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -9,27 +9,28 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        def isb(root):
-            if root.left is None and root.right is None:
-                return 1
-            if root.left:
-                l = isb(root.left)
-                if not l:
-                    return False
-            else:
-                l = 0
-            if root.right:
-                r = isb(root.right)
-                if not r:
-                    return False
-            else:
-                r = 0
-            if not (-1 <= l - r <= 1):
-                return False 
-            
-            
-            return max(l,r) + 1
-        ans = isb(root)
+        
+        ans = self.isb(root)
         if not ans:
             return False
         return True
+    def isb(self,root):
+        if root.left is None and root.right is None:
+            return 1
+        if root.left:
+            l = self.isb(root.left)
+            if not l:
+                return False
+        else:
+            l = 0
+        if root.right:
+            r = self.isb(root.right)
+            if not r:
+                return False
+        else:
+            r = 0
+        if not (-1 <= l - r <= 1):
+            return False 
+            
+            
+        return max(l,r) + 1
