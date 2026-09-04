2713be097f13ada960727c9a381ae8df80e66bea
Anirudh5477 <am25b003@smail.iitm.ac.in>
2026-09-04 12:29:48 +0530

    Time: 63 ms (68.06%) | Memory: 22.7 MB (44.51%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,29 +8,8 @@
 from collections import deque
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        qu = deque()
-        qu.append([root])
-        neededpath = [] 
-        while qu:
-            node_path = qu.popleft()
-
-            if node_path[-1] == p or node_path[-1] == q:
-                neededpath.append(node_path)
-            
-            if node_path[-1].left:
-                qu.append(node_path + [node_path[-1].left])
-
-            if node_path[-1].right:
-                qu.append(node_path + [node_path[-1].right])
-        path1 = neededpath[0]
-        path2 = neededpath[1]
-        ans = 0
-        for i in range(min(len(path1),len(path2))):
-            if path1[i] == path2[i]:
-                ans = path1[i]
-            else:
-                break
-        return ans
-                    
-            
-        
+        if p.val < root.val and q.val<root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        if p.val > root.val and q.val>root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        return root
